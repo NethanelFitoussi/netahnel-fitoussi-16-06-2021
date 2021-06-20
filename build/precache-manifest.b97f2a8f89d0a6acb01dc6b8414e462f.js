732bfb7392a89dc0fe0dfa9bd9b8f2bbdbc4b68a
nethanelfit <nethanel.fitoussi@e-quili.com>
2021-06-20 20:34:15 +0300

    build

---
+++ build/precache-manifest.b97f2a8f89d0a6acb01dc6b8414e462f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a23eb2e64ec71ba3edd9dedc963bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "622ff071f4dccee8c428",
+    "url": "/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "3f24c309c587f8293f17",
+    "url": "/static/css/main.c3859f2f.chunk.css"
+  },
+  {
+    "revision": "622ff071f4dccee8c428",
+    "url": "/static/js/2.6b895e36.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.6b895e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f24c309c587f8293f17",
+    "url": "/static/js/main.dc18bcbf.chunk.js"
+  },
+  {
+    "revision": "c5a2fe7c5031baec7088",
+    "url": "/static/js/runtime-main.62a16bae.js"
+  }
+]);
